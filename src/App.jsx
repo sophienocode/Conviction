49ecd3d821931a54dc2dd20c6893c9ae31bda90f
sophienocode <sophieprotanki@gmail.com>
2026-05-28 17:48:01 +0100

    (Auth): Building Custom Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
+import Post from './components/Post'
 
 export default function App() {
   return (
-    <div>
-      <h1> welcome to react</h1>
-    </div>
+    <>
+    <Post />
+    </>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+
+
+function Post() {
+  return (
+    <>
+      <p>This is a simple post.</p>
+      <p>React is a powerful library for building user interfaces.</p>
+    </>
+  )
+}
+
+export default Post
